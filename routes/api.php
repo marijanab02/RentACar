91a733b0a10df9224a06f38fb72dea5a03d207b1
josippreva <josip.previsic@fsre.sum.ba>
2025-05-25 16:26:19 +0200

    Resolve conflict: restore routes/api.php from stash

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserApiController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::apiResource('users', UserApiController::class);
+
+
+Route::get('/cars-api',[App\Http\Controllers\CarsApiController::class, 'index']);
+Route::get('/cars-api/{id}',[App\Http\Controllers\CarsApiController::class, 'show']);
+Route::post('/cars-api',[App\Http\Controllers\CarsApiController::class, 'store']);
+Route::put('/cars-api/{id}',[App\Http\Controllers\CarsApiController::class, 'update']);
+Route::delete('/cars-api/{id}',[App\Http\Controllers\CarsApiController::class, 'destroy']);
